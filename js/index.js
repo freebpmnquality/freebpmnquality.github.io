15fbd50a8c91ca29d92f9c3fdfd7e747f7062827
Andrii Kopp <kopp93@gmail.com>
2021-08-09 16:48:18 +0300

    Refactor JS parts.

---
+++ js/index.js
@@ -0,0 +1,10 @@
+// smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
