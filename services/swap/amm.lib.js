2dc86fb4f132467a4594d0e4faf46ce54ae42047
Andrii Kopp <kopp93@gmail.com>
2022-09-15 08:41:29 +0300

    Migration, step 0a.

---
+++ services/swap/amm.lib.js
@@ -0,0 +1,96 @@
+class AMMUtil {
+
+    static getSitemapURLs(website) {
+        const results = [];
+
+        const sitemapContent = $.ajax({
+            type: 'get',
+            url: website + '/sitemap.xml',
+            async: false
+        }).responseText;
+
+        const parser = new DOMParser();
+        const sitemapObject = parser.parseFromString(sitemapContent, 'text/xml');
+        const URLs = sitemapObject.getElementsByTagName('url');
+
+        for (let i = 0; i < URLs.length; i++) {
+            const urlElement = URLs[i];
+            const loc = urlElement.getElementsByTagName('loc')[0].textContent;
+
+            results.push(loc);
+        }
+
+        return results;
+    }
+
+    static getLiquidity() {
+        let liquidityAssets = [];
+
+        liquidityAssets = liquidityAssets.concat(AMMUtil.getSitemapURLs('https://freebpmnquality.github.io/'));
+        liquidityAssets = liquidityAssets.concat(AMMUtil.getSitemapURLs('https://cloudfreebpmnquality.herokuapp.com/'));
+
+        let liquidity = 0;
+
+        for (let asset in liquidityAssets) {
+            liquidity += EffortsLib.getValue(liquidityAssets[asset]);
+        }
+
+        return liquidity;
+    }
+
+    static getLiquidityFromStorage() {
+        return Number.parseInt(JSON.parse($.ajax({
+            type: 'get',
+            url: '../backoffice/index.php?getLiquidity=true',
+            async: false
+        }).responseText).result);
+    }
+
+    static makePrice(buy) {
+        let liquidity = AMMUtil.getLiquidityFromStorage();
+
+        const total = AMMUtil.getTotal();
+
+        return buy * (liquidity / total);
+    }
+
+    static getTotal() {
+        return Number.parseFloat(JSON.parse($.ajax({
+            type: 'get',
+            url: '../backoffice/index.php?totalSupply=true',
+            async: false
+        }).responseText).result);
+    }
+
+    static getPricing() {
+        return Number.parseInt(JSON.parse($.ajax({
+            type: 'get',
+            url: '../backoffice/index.php?getPricing=true',
+            async: false
+        }).responseText).result);
+    }
+
+    static async getMetaMaskAccounts() {
+        if (typeof window.ethereum !== 'undefined') {
+            let accounts = await ethereum.request({
+                method: 'eth_requestAccounts'
+            });
+
+            if (accounts === null) {
+                throw Error('Please install MetaMask!');
+            }
+        }
+
+        return await new Web3(window.ethereum).eth.getAccounts();
+    }
+
+    static async signMessage(message) {
+        const web3 = new Web3(window.ethereum);
+
+        const hash = web3.utils.sha3(message);
+
+        const accounts = await AMMUtil.getMetaMaskAccounts();
+
+        return await web3.eth.personal.sign(hash, accounts[0]);
+    }
+}
