b9953fe821cfe0b98f6e755aed3810ec04f06836
Andrii Kopp <kopp93@gmail.com>
2022-09-15 17:19:12 +0300

    step 4c.

--- data/ledger.js
+++ data/ledger.js
@@ -2,34 +2,34 @@ class Ledger {
     static TOKENS_DATA = '../../data/tokens.json';
 
     static getTokenData(symbol) {
-        let tokensObject = JSON.parse($.ajax({
+        let tokenObject = JSON.parse($.ajax({
             type: 'get',
             url: Ledger.TOKENS_DATA,
             async: false
         }).responseText);
 
-        tokensObject = tokensObject[symbol];
+        tokenObject = tokenObject[symbol];
 
-        tokensObject['price'] = Number.parseInt(tokensObject['price'], 16);
-        tokensObject['supply'] = Number.parseInt(tokensObject['supply'], 16);
-        tokensObject['created'] = Number.parseInt(tokensObject['created'], 16);
+        tokenObject['price'] = Number.parseInt(tokenObject['price'], 16);
+        tokenObject['supply'] = Number.parseInt(tokenObject['supply'], 16);
+        tokenObject['created'] = Number.parseInt(tokenObject['created'], 16);
 
         const timestampUNIX = Date.now() / 1000;
-        const timestampDec = (timestampUNIX - tokensObject['created']) / 86400;
+        const timestampDec = (timestampUNIX - tokenObject['created']) / 86400;
 
-        for (let address in tokensObject['holders']) {
-            if (address === tokensObject['owner']) {
-                const parameter = Number.parseInt(tokensObject['holders'][address]['parameter'], 16);
-                const constant = Number.parseInt(tokensObject['holders'][address]['constant'], 16);
+        for (let address in tokenObject['holders']) {
+            if (address === tokenObject['owner']) {
+                const parameter = Number.parseInt(tokenObject['holders'][address]['parameter'], 16);
+                const constant = Number.parseInt(tokenObject['holders'][address]['constant'], 16);
 
-                tokensObject['holders'][address] = parameter * timestampDec + constant;
+                tokenObject['holders'][address] = parameter * timestampDec + constant;
             } else {
-                tokensObject['holders'][address] = Number.parseInt(tokensObject['holders'][address], 16);
+                tokenObject['holders'][address] = Number.parseInt(tokenObject['holders'][address], 16);
             }
 
-            tokensObject['supply'] += tokensObject['holders'][address];
+            tokenObject['supply'] += tokenObject['holders'][address];
         }
 
-        return tokensObject;
+        return tokenObject;
     }
 }
